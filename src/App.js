264577049339b736d17e33f782e13f2dfd6b9d92
noeltesaluna <ntesaluna03@gmail.com>
2024-07-10 12:13:54 +1000

    Added projects.json to allow for easy extension of new projects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Project from "./components/Project";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import projects from "./data/projects.json";
 
 function App() {
   return (
@@ -44,20 +45,11 @@ function App() {
         <section id="school-projects" className="mb-8">
             <h2 className="text-lg font-semibold mb-4">School Projects</h2>
             <p className="mb-4"> Projects below are available upon request due to RMIT's academic integrity.</p>
-            
-            <Project title="Loop Cinemas" year="2023" img="loopcinemas.png">
-              Collaborated with a peer using GitHub to develop a full stack cinema website using modern web technologies such as React, GraphQL, and Sequelize with ORM capabilities. 
-              The project featured an interactive and dynamic interface, supported by a robust MySQL database managed with phpMyAdmin for efficient data management and retrieval.
-              Additionally, the website included an admin page to manage movies, reviews, and users, as well as to display statistics.
-            </Project>
-            <Project title="Super Price" year="2023" img="superprice.png">
-              Collaborated with a team of six in an Agile Scrum environment, utilizing GitHub for version control, Docker for containerization, and GitHub Actions for CI/CD. 
-              Employed Maven, Spring Boot, and JUnit to create a back-end with REST APIs, while developing a user-friendly interface using React and TypeScript for the SuperPrice website.
-            </Project>
-            <Project title="The Big Problem" year="2022" img="thebigproblem.png">
-              A static website displaying data about homelessness within Australia. Built in HTML, CSS and Javalin. 
-              Data stored and retrieved using SQLite. Utilised GitHub which allowed collaboration with a partner. 
-            </Project>
+            {projects.map((project) =>
+              <Project key={project.id} title={project.title} year={project.year} img={project.img}>
+                {project.description}
+              </Project>
+            )}
         </section>
       </main>
 
